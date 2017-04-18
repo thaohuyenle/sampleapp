949409f6d606d8a15991fbde688d647494c77616
thaohuyenle <huyenyunnie@s-Thao-Huyen.local>
2017-04-18 16:23:34 +0700

    Implement advanced login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,14 @@
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../../config/environment", __FILE__)
+require "rails/test_help"
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+end
 
-  # Add more helper methods to be used by all tests here...
+class ActionDispatch::IntegrationTest
+  def log_in_as(user, password: "password", remember_me: "1")
+    post login_path, params: { session: { email: user.email,
+     password: password, remember_me: remember_me } }
+  end
 end
